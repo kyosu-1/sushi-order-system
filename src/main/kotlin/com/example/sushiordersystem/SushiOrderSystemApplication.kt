f873a0f0757f8302b9badef7d2c6d62014b452be
kyosu-1 <sho013039@gmail.com>
2023-09-14 10:45:05 +0900

    initial spring project

---
+++ src/main/kotlin/com/example/sushiordersystem/SushiOrderSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sushiordersystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SushiOrderSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<SushiOrderSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/sushiordersystem/SushiOrderSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sushiordersystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SushiOrderSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
