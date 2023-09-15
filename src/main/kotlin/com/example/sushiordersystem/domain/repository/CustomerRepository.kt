f2c1b16c4700d240da647d92c014ca46a69f5926
kyosu-1 <sho013039@gmail.com>
2023-09-15 14:50:16 +0900

    chore: del CustomerRepository interface

--- src/main/kotlin/com/example/sushiordersystem/domain/repository/CustomerRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sushiordersystem.domain.repository
-
-import com.example.sushiordersystem.domain.model.Customer
-import java.util.UUID
-
-interface CustomerRepository {
-
-    fun createCustomer(tableId: UUID): Customer
-}
