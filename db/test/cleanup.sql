12b539e3db235c63d51cda8b153de4c95514b954
kyosu-1 <sho013039@gmail.com>
2023-09-15 15:17:14 +0900

    feat: 動作検証用のテストデータ挿入スクリプトを作成

---
+++ db/test/cleanup.sql
@@ -0,0 +1,13 @@
+-- delete all data
+
+TRUNCATE TABLE available_options;
+
+TRUNCATE TABLE customers;
+
+TRUNCATE TABLE item_options;
+
+TRUNCATE TABLE items;
+
+TRUNCATE TABLE categories;
+
+TRUNCATE TABLE tables;
