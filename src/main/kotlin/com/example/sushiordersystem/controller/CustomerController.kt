104729f76dd5c60b6858969bb8c26aa665153d7e
kyosu-1 <sho013039@gmail.com>
2023-09-20 15:59:24 +0900

    feat: openapiでレスポンスのデータ構造が見れるように

--- src/main/kotlin/com/example/sushiordersystem/controller/CustomerController.kt
+++ src/main/kotlin/com/example/sushiordersystem/controller/CustomerController.kt
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+
 
 data class CreateCustomerRequest(
         val tableId: String,
@@ -21,11 +26,19 @@ data class CreateCustomerResponse(
 @RequestMapping("/customers")
 class CustomerController(private val customerService: CustomerService) {
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "201", description = "Created",
+                        content = [Content(schema = Schema(implementation = CreateCustomerResponse::class))]),
+                ApiResponse(responseCode = "400", description = "Bad Request",
+                        content = [Content(schema = Schema(implementation = ErrorResponse::class))])
+            ]
+    )
     @PostMapping
     fun createCustomer(@RequestBody request: CreateCustomerRequest): ResponseEntity<Any> {
         return try {
             val customer = customerService.createCustomer(request.tableId)
-            ResponseEntity.ok(
+            ResponseEntity.status(201).body(
                     CreateCustomerResponse(
                             customerId = customer.customerId,
                             checkedInAt = customer.checkedInAt.toEpochMilli(),
